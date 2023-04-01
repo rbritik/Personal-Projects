d30c28044e95b42bd1fd4007d99b5946b28ed4e2
Ritik Bhardwaj <76807814+rbritik@users.noreply.github.com>
2023-04-01 23:04:51 +0530

    Update smile_selfie.py

--- Opencv/smile_selfie.py
+++ Opencv/smile_selfie.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 video = cv.VideoCapture(0)
-face_cascade = cv.CascadeClassifier(r'C:\Users\Acer\Downloads\data_science\Computer Vision\Opencv\haar-cascade-files-master\haarcascade_frontalface_alt.xml')
-smile_cascade = cv.CascadeClassifier(r'C:\Users\Acer\Downloads\data_science\Computer Vision\Opencv\haar-cascade-files-master\haarcascade_smile.xml')
+face_cascade = cv.CascadeClassifier(r'Opencv\haar-cascade-files-master\haarcascade_frontalface_alt.xml')
+smile_cascade = cv.CascadeClassifier(r'Opencv\haar-cascade-files-master\haarcascade_smile.xml')
 while True:
     ok, frame = video.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -15,7 +15,7 @@ while True:
         for x,y,w,h in smile_rect:
             frame = cv.rectangle(frame, (x,y), (x+w, y+h),(0,255,0),2)
             print("Image",str(cnt)," saved")
-            img_path = r'C:\Users\Acer\Pictures\Saved Pictures\img'+str(cnt)+'.jpg'
+            img_path = r'Saved Pictures\img'+str(cnt)+'.jpg'
             cv.imwrite(img_path,frame)
             cnt+= 1
             if cnt>=2:
